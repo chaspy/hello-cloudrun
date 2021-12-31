322784fbf80b2f5ee9814afb13d4e39a25e4c846
kondo takeshi <chaspy@users.noreply.github.com>
2022-01-01 02:28:00 +0900

    more hello

--- main.go
+++ main.go
@@ -6,7 +6,7 @@ import (
 		)
 
 		func handler(w http.ResponseWriter, r *http.Request) {
-			fmt.Fprintf(w, "Hello, Cloud Run")
+			fmt.Fprintf(w, "Hello, Cloud Run, hello")
 			}
 
 			func main() {
