a50a3aae930b07c5ce46851fd04d4e2278c12c68
kondo takeshi <chaspy@users.noreply.github.com>
2022-01-01 12:24:31 +0900

    canary

--- main.go
+++ main.go
@@ -6,7 +6,7 @@ import (
 		)
 
 		func handler(w http.ResponseWriter, r *http.Request) {
-			fmt.Fprintf(w, "Hello, Cloud Run, hello")
+			fmt.Fprintf(w, "Hello, Cloud Run, canary")
 			}
 
 			func main() {
